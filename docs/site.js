bc2683144daee3cb0336d4f05ed57d1a4c50be9b
M1croC0sm <neil.wolf@gmail.com>
2026-08-30 01:15:45 -0500

    Add Google Drive APK download link

--- docs/site.js
+++ docs/site.js
@@ -2,8 +2,7 @@ const download = document.querySelector('#drive-download');
 const checksum = document.querySelector('#checksum');
 const copy = document.querySelector('#copy-checksum');
 
-// Set these two values after the APK is uploaded to Google Drive.
-const driveUrl = '';
+const driveUrl = 'https://drive.google.com/file/d/1gvog7Q0RZFW7Qn-9jbNiZDpJMavgq7XH/view?usp=drive_link';
 const apkChecksum = 'a50973decb20f04d29267bbd11915f2f1e79d83c98171d7dda9e7d0ddb782b2b';
 
 if (driveUrl) {
